d922e2a607c6daee39e17eedea66c4241974e2bf
Dionmbaye <djinodji.dionmbaye@gmail.com>
2022-01-14 11:42:23 +0100

    add test for fizzbuzz

--- fizzbuzz.npi/fizzbuzz.npi.test/FizzBuzzNpiUnitTest.cs
+++ fizzbuzz.npi/fizzbuzz.npi.test/FizzBuzzNpiUnitTest.cs
@@ -3,11 +3,65 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace fizzbuzz.npi.test
 {
     [TestClass]
-    public class UnitTest1
+    public class FizzBuzzNpiUnitTest
     {
+        FizzBuzzNpiClass _fizzBuzzNpi = new FizzBuzzNpiClass();
         [TestMethod]
-        public void TestMethod1()
+        public void TestFizzBuzz_Should_Return_Fizz_When_6()
         {
+            //Arrange
+            var input = 6;
+
+            //Act
+            var result = _fizzBuzzNpi.FizzBuzz(input);
+
+            //Assert
+            Assert.AreEqual(result, "Fizz");
+
         }
+
+        [TestMethod]
+        public void TestFizzBuzz_Should_Return_Buzz_When_25()
+        {
+            //Arrange
+            var input = 25;
+
+            //Act
+            var result = _fizzBuzzNpi.FizzBuzz(input);
+
+            //Assert
+            Assert.AreEqual(result, "Buzz");
+
+        }
+
+        [TestMethod]
+        public void TestFizzBuzz_Should_Return_FizzBuzz_When_15()
+        {
+            //Arrange
+            var input = 15;
+
+            //Act
+            var result = _fizzBuzzNpi.FizzBuzz(input);
+
+            //Assert
+            Assert.AreEqual(result, "FizzBuzz");
+
+        }
+
+        [TestMethod]
+        public void TestFizzBuzz_Should_Return_7_When_7()
+        {
+            //Arrange
+            var input = 7;
+
+            //Act
+            var result = _fizzBuzzNpi.FizzBuzz(input);
+
+            //Assert
+            Assert.AreEqual(result, "7");
+
+        }
+
     }
+
 }
--- fizzbuzz.npi/fizzbuzz.npi/FizzBuzzNpiClass.cs
+++ fizzbuzz.npi/fizzbuzz.npi/FizzBuzzNpiClass.cs
@@ -1,7 +1,26 @@
 ﻿namespace fizzbuzz.npi
 {
-    public class Class1
+    public class FizzBuzzNpiClass
     {
+        public string FizzBuzz(int input)
+        {
+            var isMultipleDe3 = input % 3 == 0;
+            var isMultipleDe5 = input % 5 == 0;
+
+            if (isMultipleDe3 && !isMultipleDe5)
+            {
+                return "Fizz";
+            }
+            else if (isMultipleDe5 && !isMultipleDe3)
+            {
+                return "Buzz";
+            }
+            else if (isMultipleDe3 && isMultipleDe5)
+            {
+                return "FizzBuzz";
+            }
+            else return input.ToString();
+        }
 
     }
 }
